fc166b037bd696a4143f893c1c0f79bcc83b2218
watanabe kaoru <azkaoru@gmail.com>
2017-07-07 18:33:24 +0900

    add csvprinter

--- extractdef-decoder/main.go
+++ extractdef-decoder/main.go
@@ -70,38 +70,58 @@ func decode(filename string) (extractdef Extractdef, err error) {
 	return
 }
 
+//grouping patterndef.Type
+func typeGrouping(printdataList []PrintData) map[string][]PrintData {
+
+	printDataMap := make(map[string][]PrintData)
+
+	for _, printData := range printdataList {
+
+		var newList []PrintData
+		if list, ok := printDataMap[printData.Ptype]; ok {
+			newList = append(list, printData)
+		} else {
+			newList = append([]PrintData{}, printData)
+		}
+		printDataMap[printData.Ptype] = newList
+
+	}
+
+	return printDataMap
+}
+
 func main() {
 	xdef, err := decode("extractdef.xml")
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
-	fmt.Println("extractdef.xml ,PATTERNDEF size = ", len(xdef.Patterndefs))
+	//fmt.Println("extractdef.xml ,PATTERNDEF size = ", len(xdef.Patterndefs))
 
 	var data []PrintData
 
 	for _, patterndef := range xdef.Patterndefs {
 		if len(patterndef.Keywords) == 0 {
 			printdata := PrintData{
-				patterndef.Id,
 				patterndef.Type,
+				patterndef.Id,
 				patterndef.Msg.Id,
 				patterndef.Msg.Level,
 				patterndef.Name,
 				patterndef.Pattern,
-				strings.Trim(patterndef.Msg.Value, "\n "),
+				strings.Trim(strings.TrimSpace(patterndef.Msg.Value), "\t"),
 				``,
 				``}
 			data = append(data, printdata)
 		} else {
 			for _, keyword := range patterndef.Keywords {
 				printdata := PrintData{
-					patterndef.Id,
 					patterndef.Type,
+					patterndef.Id,
 					keyword.Msg.Id,
 					keyword.Msg.Level,
 					patterndef.Name,
 					patterndef.Pattern,
-					strings.Trim(keyword.Msg.Value, "\n "),
+					strings.Trim(strings.TrimSpace(keyword.Msg.Value), "\t"),
 					keyword.Pos + `:` + keyword.Pattern,
 					patterndef.Name + ` ` + keyword.Name}
 				data = append(data, printdata)
@@ -109,8 +129,10 @@ func main() {
 		}
 	}
 
-	printer := &CsvPrinter{w: os.Stdout, headPrint: true}
-	printer.print(data)
+	printDataMap := typeGrouping(data)
+	printer := &CsvPrinter{w: os.Stdout, headPrint: false}
+	for _, printdataList := range printDataMap {
+		printer.print(printdataList)
+	}
 
-	fmt.Println("extractdef.xml ,check rule size = ", len(data))
 }
